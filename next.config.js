aab4cea144ce76b707d15b632fc36a536baacf9e
RanggaBS <ranggabuanasaputra85@gmail.com>
2023-10-17 00:01:59 +0700

    .....

--- next.config.js
+++ next.config.js
@@ -18,6 +18,27 @@ const nextConfig = {
 	images: {
 		domains: ["morent-vert.vercell.app"],
 	},
+	headers: async () => {
+		return [
+			{
+				source: "/api/(.*)",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "*",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, POST, PUT, DELETE, OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type, Authorization",
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
